ed519890836b168210d84ac53b38e502ba304b9a
Angie C <a.cajusol98@gmail.com>
2025-05-23 15:21:19 +0200

    Fixa compose to run test local

--- src/config/database.js
+++ src/config/database.js
@@ -3,13 +3,23 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+let host = process.env.DB_HOST;
+let username = process.env.DB_USER;
+let password = process.env.DB_PASSWORD;
+let database = process.env.DB_NAME;
+
+if (process.env.NODE_ENV === "test") {
+  host = "localhost";
+  database = "test_db";
+}
+
 const sequelize = new Sequelize({
   dialect: "postgres",
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  logging: false, 
-  });
+  host: host,
+  username: username,
+  password: password,
+  database: database,
+  logging: false,
+});
 
 module.exports = sequelize;
