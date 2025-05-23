f00f035b60c8506e49a47cdb88b27c6e4ce3f755
Linda Zelizi <lindazelizi@gmail.com>
2025-05-23 10:55:59 +0200

    Gjort lite ändringar, innan gjorde jag unik email, nu unikt användarnamn

--- __tests__/models/user.test.js
+++ __tests__/models/user.test.js
@@ -6,12 +6,12 @@ beforeAll(async () => {
 
 describe("User Model", () => {
   it("should create a user", async () => {
-    const user = await User.create({ username: "testuser", email: "test@test.com", profilPicture: "https://cdn.pixabay.com/photo/2017/06/13/12/54/profile-2398783_1280.png" })
+    const user = await User.create({ username: "testuser", email: "test@test.com", profilePicture: "https://cdn.pixabay.com/photo/2017/06/13/12/54/profile-2398783_1280.png" })
 
     expect(user).toBeDefined();
     expect(user.username).toBe("testuser");
     expect(user.email).toBe("test@test.com");
-    expect(user.profilPicture).toBe( "https://cdn.pixabay.com/photo/2017/06/13/12/54/profile-2398783_1280.png");
+    expect(user.profilePicture).toBe( "https://cdn.pixabay.com/photo/2017/06/13/12/54/profile-2398783_1280.png");
   });
 
   it("should validate email format", async () => {
@@ -19,7 +19,14 @@ describe("User Model", () => {
     await expect(user.validate()).rejects.toThrow();
   });
 
-  it("Should be unique epost", async () => {
+  it("Should be unique email", async () => {
+    await User.create({ username: "Lisa", email: "Lisa@test.com" });
+    await expect(
+      User.create({ username: "Anders", email: "Lisa@test.com" })
+    ).rejects.toThrow();
+  });
+
+  it("Should be unique username", async () => {
     await User.create({ username: "Lisa", email: "Lisa@test.com" });
     await expect(
       User.create({ username: "Anders", email: "Anders@test.com" })
